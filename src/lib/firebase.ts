a4620aad0161c7e90be887cb1bd3da44631a652f
whoisasjad <168725808+whoisasjad@users.noreply.github.com>
2025-06-26 05:10:09 -0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // You'll need to add your Firebase config here
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyDwYruygD-9SDlb1pGwRO5X3_pauZDttBM",
+  authDomain: "blogs-test-58b9d.firebaseapp.com",
+  databaseURL: "https://blogs-test-58b9d-default-rtdb.firebaseio.com",
+  projectId: "blogs-test-58b9d",
+  storageBucket: "blogs-test-58b9d.firebasestorage.app",
+  messagingSenderId: "639855099513",
+  appId: "1:639855099513:web:ded6beea6c58e73b321ea8",
+  measurementId: "G-SRCS943N0T"
 };
 
 const app = initializeApp(firebaseConfig);
